4775c3e22f49722c585acd0eed2dd2abc4fd0eb2
NaeemAsalam <104421440+NaeemAsalam@users.noreply.github.com>
2022-06-10 16:13:04 +0500

    Add files via upload

---
+++ DemoGBA.java
@@ -0,0 +1,20 @@
+class Dog
+{
+	String color="Black";
+	String name ="Pet";
+	void Barking()
+	{
+	System.out.println("Dog is Barking");
+	}
+}
+public class DemoGBA
+{
+	public static void main(String args [])
+	{
+	Dog d1=new Dog();
+	d1.color="Black";
+	d1.name ="pet";
+	System.out.println(d1.color);
+	System.out.println(d1.name);
+	}
+}
