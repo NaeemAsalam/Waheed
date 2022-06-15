17bb12c9dab5bd2a58876896150b2361b46557f9
NaeemAsalam <104421440+NaeemAsalam@users.noreply.github.com>
2022-06-15 23:44:55 +0500

    Add files via upload

---
+++ LightSpeed.java
@@ -0,0 +1,16 @@
+class LightSpeed
+{
+	public static void main (String args[])
+	{
+	int lightspeed;
+	double days;
+	double seconds;
+	double distance;
+	days = 1000;
+	seconds = days*60*60;
+	distance = Lightspeed  * seconds;
+	System.out.println("In" + days);
+	System.out.println("days light will travel about");
+	System.out.println(distance + "miles");
+	}
+}
