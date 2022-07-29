6343f9cbff4927079a9e0ef3798a1ac35b711324
NaeemAsalam <104421440+NaeemAsalam@users.noreply.github.com>
2022-07-29 15:51:24 +0500

    Add files via upload

---
+++ MainDemoArray.java
@@ -0,0 +1,15 @@
+public class MainDemoArray
+{
+	public static void main(String[] arg)
+	{
+	int x[][]={{50,60},{70,80}};
+	for(int i=0;i<2;i++)
+	{
+	for(int j=0;j<2;j++)
+	{
+	System.out.println(x[i][j]);
+	}
+	System.out.println();
+	}
+}
+}
