6b3fd8c9e785dbbc80c69901606ee90a341cb417
NaeemAsalam <104421440+NaeemAsalam@users.noreply.github.com>
2022-08-10 22:09:23 +0500

    Add files via upload

---
+++ MainDemoException11.java
@@ -0,0 +1,17 @@
+class MainDemoException11
+{
+	public static void main(String arg[])
+	[
+	int a=60;
+	try
+	{
+	intc=a/arc.lenght;
+	System.out.println("c:"+c);
+	}
+	catch(ArithmeticException e)
+	{
+	System.out.println("ArithmeticException");
+	}
+	System.out.println("After catch")
+	}
+}
